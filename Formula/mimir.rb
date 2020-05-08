6855a3ad7b37e6238c4d9438b79b27b01e5dfb94
Le Nguyen Minh Khoi <khoilnm@dwarvesv.com>
2020-05-08 12:03:32 +0700

    init

--- Formula/mimir.rb
+++ Formula/mimir.rb
@@ -1,9 +1,9 @@
 class Mimir < Formula
   desc "Mimir"
   homepage "hhttps://github.com/mkhoi1998/mimir"
-  url "https://github.com/mkhoi1998/mimir/releases/download/v1.0.0/mimir-1.0.0.tar.gz"
+  url "https://github.com/mkhoi1998/mimir/releases/download/v1.0.0/mimir-darwin-amd64-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "e132afb1e93683f49362c53313ecbc43d0eeb750dee1621a5018d54c1c0b58aa"
+  sha256 "72c84c055990f0f0ef6b560ba1740fb555aaa9df097c05af0565f270e0816879"
 
   def install
     bin.install "mimir"
