4104a8e7c11f7a98169953fb92cab7a00bd65416
Le Nguyen Minh Khoi <khoilnm@dwarvesv.com>
2020-05-08 09:34:44 +0700

    init

--- Formula/mimir.rb
+++ Formula/mimir.rb
@@ -1,13 +1,11 @@
 class Mimir < Formula
   desc "Mimir"
   homepage "hhttps://github.com/mkhoi1998/mimir"
-  url "https://github.com/mkhoi1998/mimir/archive/v1.0.0.tar.gz"
+  url "https://github.com/mkhoi1998/mimir/releases/download/v1.0.0/mimir-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "3060ede592e4efbda7326aae0e0a0b87dbce3b63c1f11e5e6de18b1acf7f57f7"
+  sha256 "e132afb1e93683f49362c53313ecbc43d0eeb750dee1621a5018d54c1c0b58aa"
 
   def install
-    system "make", "build-mac"
-    system "ls"
-    bin.install "/mimir/build/mimir"
+    bin.install "mimir"
   end
 end
